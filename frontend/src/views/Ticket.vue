bfc723f8d0c0ff4d3910d4322b9634bf575d57cb
Alexander Bergmann <alex.bergmann87@gmail.com>
2020-06-07 17:20:38 +0200

    backendCall test

--- frontend/src/views/Ticket.vue
+++ frontend/src/views/Ticket.vue
@@ -4,6 +4,8 @@
         <div class="row">
             <div class="col-md-10 drawer" id="welcome">
                 <h1>Hier können bald Tickets für Bus und Bahn erworben werden!</h1>
+                <button class="primary" @click="testBackendCall()">TestBackendCall</button>
+                <p v-if="this.text != ''">{{this.text}}</p>
             </div>
             <TheSidebar />
         </div>
@@ -11,6 +13,7 @@
 </template>
 
 <script>
+import axios from 'axios';
 import TheHeader from '../components/TheHeader.vue';
 import TheSidebar from '../components/TheSidebar.vue';
 
@@ -19,6 +22,23 @@ export default {
     components: {
         TheHeader,
         TheSidebar,
+    },
+    data() {
+        return {
+            text: '',
+        };
+    },
+    methods: {
+        testBackendCall() {
+            axios.get('http://opnv.dvess.network/api/api')
+                .then((response) => {
+                    console.log(response.data);
+                    this.text = response.data;
+                })
+                .catch((e) => {
+                this.errors.push(e);
+                });
+        },
     },
 };
 </script>
